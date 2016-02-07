a3c059e018d74e7a78b4f0773aa599f77a29ca0a
Stevelopment <stevelopment@Steves-MacBook-Pro.local>
2016-02-06 20:37:30 -0800

    Basic templating

--- stevesite/portfolio/views.py
+++ stevesite/portfolio/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return render(request, "portfolio/index.html")
+	context = {
+		'name':'Steve',
+		'title':'Hello world',
+		'list':['a', 'list', 'of', 'words']
+	}
+	return render(request, "portfolio/index.html", context)
